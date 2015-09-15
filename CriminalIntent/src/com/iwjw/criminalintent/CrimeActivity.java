bd71e5872a984b790134697eefbe0da41bcd8eb1
wfeiyang <wfeiyangvip@163.com>
2015-09-15 23:40:30 +0800

    reAdd

---
+++ CriminalIntent/src/com/iwjw/criminalintent/CrimeActivity.java
@@ -0,0 +1,15 @@
+package com.iwjw.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class CrimeActivity extends FragmentActivity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+}
