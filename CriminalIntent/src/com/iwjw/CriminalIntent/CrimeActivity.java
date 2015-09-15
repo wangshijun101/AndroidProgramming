7bf3ffb9797cb809216b9c34b91087170de8047c
wfeiyang <wfeiyangvip@163.com>
2015-09-15 23:00:37 +0800

    Init modules

---
+++ CriminalIntent/src/com/iwjw/CriminalIntent/CrimeActivity.java
@@ -0,0 +1,15 @@
+package com.iwjw.CriminalIntent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CrimeActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+}
